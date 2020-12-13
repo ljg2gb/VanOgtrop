23654a77553904dda52305bd1ded2300fcde11a9
Lydia Gregory <ljg2gb@virginia.edu>
2020-12-12 23:55:48 -0500

    sets up boilerplate app with folder structure, sass, garamond, and nav ccomponent

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,8 @@
 module.exports = {
+  siteMetadata: {
+    title: `Kristin Van Ogtrop Website`,
+    description: `Author of Let Me Lay Down and Did I Say That Outloud and former editor-in-cheif of Real Simple.`,
+    author: `Lydia Gregory`, 
+  },
   plugins: ["gatsby-plugin-sass"],
 };
---
+++ src/pages/components/nav.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function Nav() {
+    return (
+      <nav>
+        <h1>Kristin Van Ogtrop</h1>
+        <div className="header-links-container">
+            <p className="header-link">About</p>
+            <p className="header-link">Books</p>
+            <p className="header-link">Miscellany</p>
+            <p className="header-link">Press</p>
+            <p className="header-link">Events</p>
+            <p className="header-link">Contact</p>
+        </div>
+      </nav>
+    )
+  }
