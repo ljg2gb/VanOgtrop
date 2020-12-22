c141faaa2643fc8f364fcc5a42cf3b36a08032e2
Lydia Gregory <ljg2gb@virginia.edu>
2020-12-21 23:52:16 -0500

    adds books to books page

--- src/pages/books.js
+++ src/pages/books.js
@@ -2,6 +2,8 @@ import React from "react";
 import { Link } from "gatsby";
 import Nav from '../components/nav';
 import Footer from '../components/footer';
+import JustLetMe from '../../assets/JustLetMe.jpg'
+import DidISayThatOutloud from '../../assets/DidISayThatOutloud.jpg'
 
 export default function Books() {
   return (
@@ -9,7 +11,18 @@ export default function Books() {
         <Nav/>
         <div className='main'>
             <h1>Books</h1>
-            <Link to="/book">Book Component</Link>
+            <div className="page-content">
+                <Link to="/book" className="book">
+                    <img src={JustLetMe} alt="Book cover: Just Let Me Lie Down"></img>
+                </Link>
+                <Link to="/book" className="book">
+                    <img src={DidISayThatOutloud} alt="Book cover: Did I Say That Outloud?"></img>
+                </Link>
+                <h4>“Van Ogtrop nails with a generous, wisecracking charm what we couldn’t —even with sleep- articulate.” 
+                    — Elissa Schappell, 
+                    Vanity Fair
+                </h4>
+            </div>
         </div>
         <Footer/>
     </div>
