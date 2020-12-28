f7ca74f47325ca0317a89291d1ab631283ce9d44
Lydia Gregory <ljg2gb@virginia.edu>
2020-12-27 22:59:49 -0500

    updates content with edits from Kristin

--- src/pages/miscellany.js
+++ src/pages/miscellany.js
@@ -11,59 +11,59 @@ export default function Miscellany() {
             <h1>MISCELLANY</h1>
             <div className= 'page-content'>
                 <div className='text-content-left'>
-                    <h3>A random sampling of things I’ve written over the years.</h3>
+                    <h3><i>Random stuff, written by me</i></h3>
                     <br/>
                     <p><a href='https://github.com/ljg2gb/Candle' target="_blank" rel="noreferrer">The Difference Between Men and Women, a.k.a. The “Lice Epidemic”</a></p>
-                    <p>Real Simple</p>
+                    <p><i>Real Simple</i></p>
                     <br/>
                     <p><a href='https://github.com/ljg2gb/Candle' target="_blank" rel="noreferrer">Why Ambition Isn’t Working for Women</a></p>
-                    <p>Time</p>
+                    <p><i>Time</i></p>
                     <br/>
                     <p><a href='https://github.com/ljg2gb/Candle' target="_blank" rel="noreferrer">Why I’d Rather Stand Up Straight Than Lean In</a></p>
-                    <p>Huffpost</p>
-                    <br/>
-                    <p><a href='https://github.com/ljg2gb/Candle' target="_blank" rel="noreferrer">After the World Cup: Looking for Reasons to Go On</a></p>
-                    <p>Real Simple</p>
-                    <br/>
-                    <p><a href='https://github.com/ljg2gb/Candle' target="_blank" rel="noreferrer">The Most Important Difference Between an Elite Athlete and a Middle-Aged Writer</a></p>
-                    <p>Time</p>
-                    <br/>
-                    <p><a href='https://github.com/ljg2gb/Candle' target="_blank" rel="noreferrer">What It’s Like to Give Your Dog a Personality Test</a></p>
-                    <p>Real Simple</p>
-                    <br/>
-                    <p><a href='https://github.com/ljg2gb/Candle' target="_blank" rel="noreferrer">A Son Has Gone to College, Leaving His Adoring Mother Behind…Wait, Did I Have Three Kids?</a></p>
-                    <p>Real Simple</p>
-                    <br/>
-                    <p><a href='https://github.com/ljg2gb/Candle' target="_blank" rel="noreferrer">How this Family of Worrywarts Copes In an Age of Anxiety</a></p>
-                    <p>Time</p>
-                    <br/>
-                    <p><a href='https://github.com/ljg2gb/Candle' target="_blank" rel="noreferrer">Invasion of the Garden Snatcher and Other Tales of Suburban Apocalypse</a></p>
-                    <p>Time</p>
-                    <br/>
-                    <p><a href='https://github.com/ljg2gb/Candle' target="_blank" rel="noreferrer">Hey! You! Get Off of My Cloud! And Other Tales from the Family-Data-Sharing Economy</a></p>
-                    <p>Time</p>
-                    <br/>
-                    <p><a href='https://github.com/ljg2gb/Candle' target="_blank" rel="noreferrer">What Do I Do Now? A Midlife Career Change May Be Just the Challenge You Need</a></p>
-                    <p>Time</p>
-                    <br/>
-                    <p><a href='https://github.com/ljg2gb/Candle' target="_blank" rel="noreferrer">My Household Get-out-the-vote Campaign Hits a Teen Roadblock</a></p>
-                    <p>Time</p>
-                    <br/>
-                    <p><a href='https://github.com/ljg2gb/Candle' target="_blank" rel="noreferrer">My Last Year in Office as Mom-in-Chief</a></p>
-                    <p>Time</p>
-                    <br/>
-                    <p><a href='https://github.com/ljg2gb/Candle' target="_blank" rel="noreferrer">Reality Check: A Rallying Cry Against Faux-Perfection on Social Media</a></p>
-                    <p>Huffpost</p>
-                    <br/>
-                    <p><a href='https://github.com/ljg2gb/Candle' target="_blank" rel="noreferrer">We Are All Ordinary, and We Are All Superheroes</a></p>
-                    <p>Huffpost</p>
+                    <p><i>HuffPost</i></p>
                     <br/>
                     <p><a href='https://github.com/ljg2gb/Candle' target="_blank" rel="noreferrer">Head Case</a></p>
-                    <p>Vogue</p>
+                    <p><i>Vogue</i></p>
+                    <br/>
+                    <p><a href='https://github.com/ljg2gb/Candle' target="_blank" rel="noreferrer">After the World Cup: Looking for Reasons to Go On</a></p>
+                    <p><i>Real Simple</i></p>
+                    <br/>
+                    <p><a href='https://github.com/ljg2gb/Candle' target="_blank" rel="noreferrer">The Most Important Difference Between an Elite Athlete and a Middle-Aged Writer</a></p>
+                    <p><i>Time</i></p>
+                    <br/>
+                    <p><a href='https://github.com/ljg2gb/Candle' target="_blank" rel="noreferrer">What It’s Like to Give Your Dog a Personality Test</a></p>
+                    <p><i>Real Simple</i></p>
+                    <br/>
+                    <p><a href='https://github.com/ljg2gb/Candle' target="_blank" rel="noreferrer">A Son Has Gone to College, Leaving His Adoring Mother Behind…Wait, Did I Have Three Kids?</a></p>
+                    <p><i>Real Simple</i></p>
+                    <br/>
+                    <p><a href='https://github.com/ljg2gb/Candle' target="_blank" rel="noreferrer">How this Family of Worrywarts Copes In an Age of Anxiety</a></p>
+                    <p><i>Time</i></p>
+                    <br/>
+                    <p><a href='https://github.com/ljg2gb/Candle' target="_blank" rel="noreferrer">Invasion of the Garden Snatcher and Other Tales of Suburban Apocalypse</a></p>
+                    <p><i>Time</i></p>
+                    <br/>
+                    <p><a href='https://github.com/ljg2gb/Candle' target="_blank" rel="noreferrer">Hey! You! Get Off of My Cloud! And Other Tales from the Family-Data-Sharing Economy</a></p>
+                    <p><i>Time</i></p>
+                    <br/>
+                    <p><a href='https://github.com/ljg2gb/Candle' target="_blank" rel="noreferrer">What Do I Do Now? A Midlife Career Change May Be Just the Challenge You Need</a></p>
+                    <p><i>Time</i></p>
+                    <br/>
+                    <p><a href='https://github.com/ljg2gb/Candle' target="_blank" rel="noreferrer">My Household Get-out-the-vote Campaign Hits a Teen Roadblock</a></p>
+                    <p><i>Time</i></p>
+                    <br/>
+                    <p><a href='https://github.com/ljg2gb/Candle' target="_blank" rel="noreferrer">My Last Year in Office as Mom-in-Chief</a></p>
+                    <p><i>Time</i></p>
+                    <br/>
+                    <p><a href='https://github.com/ljg2gb/Candle' target="_blank" rel="noreferrer">Reality Check: A Rallying Cry Against Faux-Perfection on Social Media</a></p>
+                    <p><i>HuffPost</i></p>
+                    <br/>
+                    <p><a href='https://github.com/ljg2gb/Candle' target="_blank" rel="noreferrer">We Are All Ordinary, and We Are All Superheroes</a></p>
+                    <p><i>HuffPost</i></p>
                </div>
                 <div className='media-content-right'>
                     <img src={Waffle} alt="Pureed hospital waffle molded back to a waffle"></img>
-                    <caption>Do you think this is a waffle?  Ha!  It’s what a hospital kitchen sends to try to cheer you up, or maybe trick you, when you can only eat pureed food.  See my second book for details.</caption>
+                    <caption><i>See</i> Did I Say That Out Loud <i>for details.</i></caption>
                 </div>
             </div>
         </div>
--- src/pages/press.js
+++ src/pages/press.js
@@ -12,32 +12,32 @@ export default function Press() {
             <div className= 'page-content'>
                 <div className='text-content-left'>
                     <p><a href='https://github.com/ljg2gb/Candle' target="_blank" rel="noreferrer">Midlife Moms</a></p>
-                    <p>Chicago Tribune</p>
+                    <p><i>Chicago Tribune</i></p>
                     <br/>
                     <p><a href='https://github.com/ljg2gb/Candle' target="_blank" rel="noreferrer">Former Real Simple Editor Offers Darden Students Advice on Work-Life Balance</a></p>
-                    <p>University of Virginia</p>
+                    <p><i>UVA Today</i></p>
                     <br/>
                     <p><a href='https://github.com/ljg2gb/Candle' target="_blank" rel="noreferrer">Kristin Van Ogtrop on Surviving the Holidays</a></p>
-                    <p>Washington Post</p>
+                    <p><i>Washington Post</i></p>
                     <br/>
                     <p><a href='https://github.com/ljg2gb/Candle' target="_blank" rel="noreferrer">The 60-second interview: Kristin van Ogtrop, Editor, Real Simple</a></p>
-                    <p>Politico</p>
+                    <p><i>Politico</i></p>
                     <br/>
                     <p><a href='https://github.com/ljg2gb/Candle' target="_blank" rel="noreferrer">Longtime Real Simple Editor Steps Down Amid Changes at Time Inc.</a></p>
-                    <p>AdWeek</p>
+                    <p><i>AdWeek</i></p>
                     <br/>
                     <p><a href='https://github.com/ljg2gb/Candle' target="_blank" rel="noreferrer">Real Simple’s Editor in Chief Kristin van Ogtrop Departs Time Inc.</a></p>
-                    <p>WWD</p>
+                    <p><i>WWD</i></p>
                     <br/>
                     <p><a href='https://github.com/ljg2gb/Candle' target="_blank" rel="noreferrer">Top 5: Kristin van Ogtrop</a></p>
-                    <p>Westchester Magazine</p>
+                    <p><i>Westchester Magazine</i></p>
                     <br/>
                     <p><a href='https://github.com/ljg2gb/Candle' target="_blank" rel="noreferrer">Outdoor Entertaining Made Simple</a></p>
-                    <p>Virginia Magazine</p>
+                    <p><i>Virginia Magazine</i></p>
                </div>
                 <div className='media-content-right'>
                     <img src={Ina} alt="Sitting with the Barefoot Contessa"></img>
-                    <caption>One time when I was the editor in chief of Real Simple I got to sit next to Ina Garten at a fancy dinner which means I can now die happy!</caption>
+                    <caption>Once, during my years as a magazine editor, I got to sit next to Ina Garten at a fancy dinner.  She was just as warm and wonderful as I had always imagined, and now I can die happy.</caption>
                 </div>
             </div>
         </div>
