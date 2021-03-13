e42d6b0df9030b17c1cf9f69b15196749e63c6cd
Lydia Gregory <ljg2gb@virginia.edu>
2021-03-13 15:52:55 -0500

    JustLetMe content done

--- src/pages/JustLetMe.js
+++ src/pages/JustLetMe.js
@@ -2,75 +2,78 @@ import React from "react";
 import Nav from '../components/nav';
 import Footer from '../components/footer';
 import JustLetMe from '../../assets/JustLetMe.jpg'
+import { graphql } from 'gatsby';
+import { RichText } from 'prismic-reactjs';
 
-export default function JustLetMeLieDown() {
+export const JustLetMeLieDown = ({data})  => {
+  if (!data) return null
+  const document = data.prismicBookPage.data
   return (
     <div>
       <Nav/>
         <div className='main'>
-            <h1>Just Let Me Lie Down</h1>
+        <h1>{document.title.text}</h1>
             <div className="page-content">
               <div className='media-content-left'>
                 <div className="book">
-                    <img src={JustLetMe} alt="Book cover: Just Let Me Lie Down"></img>
+                    <img src={document.image.url} alt={document.image.alt}></img>
                 </div>
               </div>
               <div className='text-content-right'>
-                <h2>Purchase Now</h2>
+                <h2>{document.heading_2.text}</h2>
                 <div>
-                <button>
-                    <a href='https://www.northshire.com/book/9780316068291'
+                  <button>
+                    <a href={document.buttons[0].buttonlink.url}  
                         className="button-text"
                         target="_blank" 
                         rel="noreferrer">
-                        Northshire Books
+                        {document.buttons[0].buttontext.text}
                     </a>
                   </button>
                   <button>
-                    <a href='https://www.indiebound.org/book/9780316068291'
+                    <a href={document.buttons[1].buttonlink.url} 
                         className="button-text"
                         target="_blank" 
                         rel="noreferrer">
-                        IndieBound
+                        {document.buttons[1].buttontext.text} 
                     </a>
                   </button>
                   <button>
-                    <a href='https://www.amazon.com/Just-Let-Lie-Down-Half-Insane/dp/0316068292/ref=sr_1_1?dchild=1&keywords=just+let+me+lie+down+kristin+van+ogtrop&qid=1611517750&sr=8-1'  
+                    <a href={document.buttons[2].buttonlink.url} 
                         className="button-text"
                         target="_blank" 
                         rel="noreferrer">
-                        Amazon
+                        {document.buttons[2].buttontext.text} 
                     </a>
                   </button>
                   <button>
-                    <a href='https://www.barnesandnoble.com/w/just-let-me-lie-down-kristin-van-ogtrop/1100053605?ean=9780316068291'  
+                    <a href={document.buttons[3].buttonlink.url}   
                         className="button-text"
                         target="_blank" 
                         rel="noreferrer">
-                        Barnes & Noble
+                        {document.buttons[3].buttontext.text} 
                     </a>
                   </button>
                   <button>
-                    <a href='https://www.target.com/p/just-let-me-lie-down-by-kristin-van-ogtrop-paperback/-/A-79261684#lnk=sametab'  
-                        className="button-text"
+                    <a href={document.buttons[4].buttonlink.url} 
                         target="_blank" 
                         rel="noreferrer">
-                        Target
+                        {document.buttons[4].buttontext.text} 
                     </a>
                   </button>
                   <button>
-                    <a href='https://www.walmart.com/ip/Just-Let-Me-Lie-Down-Necessary-Terms-for-the-Half-Insane-Working-Mom-Paperback-9780316068291/15182939'  
+                    <a href={document.buttons[5].buttonlink.url} 
                         className="button-text"
                         target="_blank" 
                         rel="noreferrer">
-                        Walmart
+                        {document.buttons[5].buttontext.text} 
                     </a>
                   </button>
                 </div>
                 <br/>
-                <h3><i>“A hysterical and touching handbook for frantic working moms.”</i> — Self magazine</h3>
-                <h3 className="mb-1"><i>“A witty lexicon of marriage and motherhood.”</i> — Vogue</h3>
-                <p>Kristin van Ogtrop knows she’s lucky-fulfilling career, great husband, three healthy kids, and, depending on the hamster count, an impressive roster of pets. You could also say she’s half-insane, but name one working mom who isn’t.  Using stories and insights from her own life, van Ogtrop offers a lexicon for working moms everywhere. Terms and concepts illustrate the highs (kids who know where their soccer cleats are, coworkers who don’t hit “Reply All,” dogs who helpfully eat whatever falls from the table) and the lows (getting out of the house in the morning, getting along with everyone at the office, getting willful kids into bed) of balancing work and family.</p>
+                <RichText render={document.reviews[0].review.raw}/>
+                <RichText class="mb-1" render={document.reviews[1].review.raw}/>
+                <RichText render={document.summary.raw}/>
               </div>  
             </div>
         </div>
@@ -78,3 +81,41 @@ export default function JustLetMeLieDown() {
     </div>
   )
 }
+
+export const query = graphql`
+query JustLetMeQuery {
+  prismicBookPage(uid: {eq: "just-let-me-lie-down"}) {
+    uid
+    data {
+      title {
+        text
+      }
+      summary {
+        raw
+      }
+      reviews {
+        review {
+          raw
+        }
+      }
+      image {
+        url
+        alt
+      }
+      heading_2 {
+        text
+      }
+      buttons {
+        buttonlink {
+          url
+        }
+        buttontext {
+          text
+        }
+      }
+    }
+  }
+}
+`
+
+export default JustLetMeLieDown;
